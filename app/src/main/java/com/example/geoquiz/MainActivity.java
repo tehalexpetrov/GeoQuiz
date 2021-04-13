2aa03cb4aeaf666dd57f315eef5bb50ad32606f4
Alexander <tehalexpetrov@gmail.com>
2021-04-14 00:07:33 +0300

    Добавил новую активити

--- app/src/main/java/com/example/geoquiz/MainActivity.java
+++ app/src/main/java/com/example/geoquiz/MainActivity.java
@@ -9,6 +9,8 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.concurrent.atomic.AtomicInteger;
@@ -21,8 +23,11 @@ public class MainActivity extends AppCompatActivity {
     private ImageButton mBackButton;
     private TextView mQuestionTextView;
     private int mCurrentIndex = 0;
+    private int tryCount = 0; //Счетчик правильных ответов
+    private int falseCount = 0; //Счетчик не правильных ответов
 
     public static  final String TAG = "MainActivity"; //Логирование
+    public static final String KEY_INDEX = "index";
 
     private Questions[] mQuestionBank = new Questions[]{
             new Questions(R.string.question_Australia, true),
@@ -30,17 +35,7 @@ public class MainActivity extends AppCompatActivity {
             new Questions(R.string.question_Africa, true),
     };
 
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-    }
-
+    private boolean[] mQuestionsAnswered = new boolean[mQuestionBank.length];
 
 
     @Override
@@ -48,6 +43,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         Log.d(TAG, "onCreate(Bundle) called"); //Логирование onCreate
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState != null) {
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+            mQuestionsAnswered = savedInstanceState.getBooleanArray(KEY_INDEX);
+        }
+
 
         //Поле вывода вопроса. А так же возможность выбрать другой вопрос
         mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
@@ -62,13 +63,14 @@ public class MainActivity extends AppCompatActivity {
 //Кнопка. Верный ответ
         mTrueButton = (Button) findViewById(R.id.true_button);
         mTrueButton.setOnClickListener(v -> {
-            checkAnswer(true); // Проверка на правильность ответа на вопрос
+            checkAnswer(true);// Проверка на правильность ответа на вопрос
         });
 
 //Кнопка. Неверный ответ
         mFalseButton = (Button) findViewById(R.id.false_button);
         mFalseButton.setOnClickListener(v -> {
             checkAnswer(false);
+            mFalseButton.setEnabled(!mQuestionsAnswered[mCurrentIndex]);
         });
 //Добавил кнопку вперед
         mNextButton = (ImageButton) findViewById(R.id.next_button);
@@ -92,6 +94,14 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle saveInstanceState) {
+        super.onSaveInstanceState(saveInstanceState);
+        Log.i(TAG, "saveInstanceState");
+        saveInstanceState.putInt(KEY_INDEX, mCurrentIndex);
+        saveInstanceState.putBooleanArray(KEY_INDEX, mQuestionsAnswered);
+    }
+
     private void updateQuestion() {
         AtomicInteger question = new AtomicInteger(mQuestionBank[mCurrentIndex].getTextResId());
         mQuestionTextView.setText(question.get());
@@ -102,9 +112,17 @@ public class MainActivity extends AppCompatActivity {
 
         int messageResId = 0;
 
+        mQuestionsAnswered[mCurrentIndex] = true;
+        mTrueButton.setEnabled(false);
+        mFalseButton.setEnabled(false);
+
+        mTrueButton.setEnabled(!mQuestionsAnswered[mCurrentIndex]);
+        mFalseButton.setEnabled(!mQuestionsAnswered[mCurrentIndex]);
+
         if (userPressedTrue == answerIsTrue) {
             messageResId = R.string.correct_answer;
         } else {
+            mFalseButton.setEnabled(false);
             messageResId = R.string.incorrect_answer;
         }
         Toast toast = Toast.makeText(this, messageResId, Toast.LENGTH_SHORT);
